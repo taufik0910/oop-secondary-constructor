3fd7ea3ec4fd0afafe60f48a241231e392de93fc
taufik0910 <taufik.cradle@gmail.com>
2022-08-01 00:45:53 +0700

    pemanggilan class murid dalam class main

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    //pemanggilan class Murid dalam Class main
+val obj = Murid("taufik",32)
+
+
+
+
+
+
+
+
+
+//    println("Hello World!")
+//
+//    // Try adding program arguments via Run/Debug configuration.
+//    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+//    println("Program arguments: ${args.joinToString()}")
+}
---
+++ src/main/kotlin/Murid.kt
@@ -0,0 +1,8 @@
+class Murid {
+    //secondary constructor initial, ditambahkan dengan method constructor, seperti yang terlampir dibawah
+
+    constructor(nama : String, umur : Int){
+        println( "Nama Murid nya : ${nama.toUpperCase()}")
+        println("Umur muridnya : $umur ")
+    }
+}
