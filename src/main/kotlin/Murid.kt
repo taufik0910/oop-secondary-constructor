68b2f95afe73bc0831efcbb067a556667f958aa2
taufik0910 <taufik.cradle@gmail.com>
2022-08-01 01:12:51 +0700

    penambahan function standard library kotlin(constructor)

--- src/main/kotlin/Murid.kt
+++ src/main/kotlin/Murid.kt
@@ -1,6 +1,5 @@
 class Murid {
     //secondary constructor initial, ditambahkan dengan method constructor, seperti yang terlampir dibawah
-
     constructor(nama : String, umur : Int){
         println( "Nama Murid nya : ${nama.toUpperCase()}")
         println("Umur muridnya : $umur ")
